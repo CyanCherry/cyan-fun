e8a8cfd3cbc764b8147c0628236c7d9c8862ed97
Cyan Cherry <cyan_cherry@qq.com>
2022-06-19 15:05:27 +0800

    feat: custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,15 @@
-import styles from "./index.module.scss"
+import styles from "./404.module.scss"
+import Link from "next/link"
 
 function Games() {
   return (
     <div className={styles.background}>
+      <div className={styles.tip}>
+        You are MISSED...
+        <Link href="/">
+          <span className={styles.home}>go Home?</span>
+        </Link>
+      </div>
       <div className={styles.container}>
         <div className={styles.lint}/>
         <div className={styles.circle}/>
